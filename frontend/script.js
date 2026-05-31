077dd678b80cab8adf3de58825a2bb14e3584601
Prem1737 <eprem1737@gmail.com>
2026-05-31 15:55:24 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,28 @@
-function calculateTrust(){
+async function predict() {
 
-    let score = Math.floor(Math.random() * 100) + 1;
+    const transparency = document.getElementById("transparency").value;
+    const fairness = document.getElementById("fairness").value;
+    const privacy = document.getElementById("privacy").value;
+    const security = document.getElementById("security").value;
+    const explainability = document.getElementById("explainability").value;
+
+    const response = await fetch(
+        `http://127.0.0.1:5000/predict?transparency=${transparency}&fairness=${fairness}&privacy=${privacy}&security=${security}&explainability=${explainability}`
+    );
+
+    const data = await response.json();
+
+    let level = "";
+
+    if (data.trust_score >= 80) {
+        level = " HIGH TRUST";
+    } else if (data.trust_score >= 50) {
+        level = " MEDIUM TRUST";
+    } else {
+        level = " LOW TRUST";
+    }
 
     document.getElementById("result").innerHTML =
-    "Trust Score: " + score + "%";
+        "Trust Score: " + data.trust_score +
+        "<br><br>" + level;
 }
